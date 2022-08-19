53f0ad07398587bf13b29867344044e743ed44ed
MarkoAyala <markoayala3@hotmail.com>
2022-08-19 00:33:17 -0300

    reducer

---
+++ Ecom/frontend/src/app/Interfaces/interfaceImages.d.ts
@@ -0,0 +1,13 @@
+export interface Images{
+    _id:string
+    name:string
+    url:string
+    product_id:string
+}
+
+export const ImagesTemplate = {
+    _id:'',
+    name:'',
+    url:'',
+    product_id:''
+}
--- Ecom/frontend/src/app/Reducers/ImagesSlice.ts
+++ Ecom/frontend/src/app/Reducers/ImagesSlice.ts
@@ -1,3 +1,44 @@
 import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from 'axios';
+import { Images, ImagesTemplate } from "../Interfaces/interfaceImages";
 import { AsyncThunk } from "@reduxjs/toolkit";
+
+
+interface InitialState {
+    images:Array<Images>
+}
+
+const initialState:InitialState = {
+    images:[ImagesTemplate]
+}
+
+export const fetchImages:AsyncThunk<any , void , {}> = createAsyncThunk(
+    'images/GETimages',
+    async()=> {
+        try{
+            const response = (await axios('/images')).data;
+            return response;
+        }catch(err){
+            if(err instanceof Error){
+                console.log(err.message);
+            }else{
+                console.log("Unexpected error", err);
+            }
+        }
+    }
+)
+
+export const imagesSlice = createSlice({
+    name:'images',
+    initialState,
+    reducers:{
+
+    },
+    extraReducers:(builder) => {
+        builder.addCase(fetchImages.fulfilled, (state,action)=>{
+            state.images = action.payload
+        });
+    }
+});
+
+export default imagesSlice.reducer;
