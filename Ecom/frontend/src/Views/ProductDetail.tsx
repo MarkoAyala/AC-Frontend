4b1bf058f6dc38f204f0fce72e12cc937846e13b
MarkoAyala <markoayala3@hotmail.com>
2022-09-23 17:11:54 -0300

    swiper

--- Ecom/frontend/src/Views/ProductDetail.tsx
+++ Ecom/frontend/src/Views/ProductDetail.tsx
@@ -3,11 +3,19 @@ import { useParams } from "react-router-dom";
 // ========= IMPORT MUI COMPONENTS ========== //
 import { Grid , Box, Skeleton , Button } from '@mui/material';
 // ============= IMPORT UTILITIES ======== //
+import {Swiper , SwiperSlide} from 'swiper/react';
+import SwiperCore, {Keyboard , Scrollbar,Pagination , Navigation,} from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { fetchProductById } from "../app/Reducers/productByIdSlice";
 import { Product } from "../app/Interfaces/interfaceProducts";
 import { ProductTemplate } from "../app/Utils/postProduct";
 import { getProductById } from "../app/Utils/getProductById";
+
+SwiperCore.use([Keyboard,Scrollbar,Pagination,Navigation])
+
 function ProductDetail(){
     const producto:any = useAppSelector<any>((state:any)=> state.productById.productById);
     const dispatch:any = useAppDispatch();
@@ -20,8 +28,8 @@ function ProductDetail(){
     if(producto[0] && producto[0].name !==''){
 
         return (
-            <Grid container sx={{width:'100%', display:'flex', justifyContent:'center', marginTop:'2rem'}}>
-                <Grid container sx={{width:'80%',marginTop:'8rem', background:'var(--azulOscuro)', color:'white', borderRadius:'10px'}}>
+            <Grid container sx={{width:'100%', display:{xs:'none', md:'flex'}, justifyContent:'center', marginTop:'2rem'}}>
+                <Grid container sx={{width:{xs:'96%',xl:'80%'},marginTop:'8rem', background:'var(--azulOscuro)', color:'white', borderRadius:'10px'}}>
     
                     {/* Container miniaturas */}
     
@@ -55,11 +63,33 @@ function ProductDetail(){
     
                     {/* Imagen principal */}
     
-                    <Grid item xs={11}>
-                        <Box sx={{width:'60%',height:'100%', display:'flex', justifyContent:'center', background:'rgba(0,0,0,0.4)'}}>
+                    <Grid item xs={4} width='478px !important'>
+                        <Box sx={{width:'100%',height:'100%', display:'flex !important', justifyContent:'center !important', background:'rgba(0,0,0,0.4)'}}>
                             {
                                 producto &&producto[0] && producto[0].name !== ''?(
-                                    <img src={producto[0].url.img1} alt='noimg' style={{maxWidth:'100%', height:'600px', width:'382px', objectFit:'cover'}} />
+                                    <Swiper
+                                    grabCursor
+                                    keyboard={{enabled:true}}
+                                    pagination={{clickable:true}}
+                                    navigation
+                                    loop
+                                    style={{padding:'0 0 3rem 0',}}
+                                    >
+                                        {
+                                            producto[0].url?.img1?(
+                                            <SwiperSlide>
+                                                <img src={producto[0].url.img1} alt='noimg' style={{maxWidth:'100%', height:'600px', width:'382px', objectFit:'cover'}} />
+                                            </SwiperSlide>
+                                            ):null
+                                        }
+                                        {
+                                            producto[0].url?.img2?(
+                                                <SwiperSlide>
+                                                    <img src={producto[0].url.img2} alt='noimg' style={{maxWidth:'100%', height:'600px', width:'382px', objectFit:'cover'}} />
+                                                </SwiperSlide>
+                                                ):null
+                                        }
+                                    </Swiper>
                                 ):null
                             }
                         </Box>
