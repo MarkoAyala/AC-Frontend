65a63a0e0dc484c0e648064df24c8573392d90b2
MarkoAyala <markoayala3@hotmail.com>
2022-09-25 21:10:12 -0300

    swiper mobile

--- Ecom/frontend/src/Views/ProductDetail.tsx
+++ Ecom/frontend/src/Views/ProductDetail.tsx
@@ -13,6 +13,7 @@ import { fetchProductById } from "../app/Reducers/productByIdSlice";
 import { Product } from "../app/Interfaces/interfaceProducts";
 import { ProductTemplate } from "../app/Utils/postProduct";
 import { getProductById } from "../app/Utils/getProductById";
+import TittleEfect from "../Components/TitleEffect/TittleEfect";
 
 SwiperCore.use([Keyboard,Scrollbar,Pagination,Navigation])
 
@@ -28,6 +29,7 @@ function ProductDetail(){
     if(producto[0] && producto[0].name !==''){
 
         return (
+            <>
             <Grid container sx={{width:'100%', display:{xs:'none', md:'flex'}, justifyContent:'center', marginTop:'2rem'}}>
                 <Grid container sx={{width:{xs:'96%',xl:'80%'},marginTop:'8rem', background:'var(--azulOscuro)', color:'white', borderRadius:'10px'}}>
     
@@ -77,15 +79,15 @@ function ProductDetail(){
                                     >
                                         {
                                             producto[0].url?.img1?(
-                                            <SwiperSlide>
-                                                <img src={producto[0].url.img1} alt='noimg' style={{maxWidth:'100%', height:'600px', width:'382px', objectFit:'cover'}} />
+                                            <SwiperSlide style={{display:'flex', justifyContent:''}}>
+                                                <img className="pcSwiper" src={producto[0].url.img1} alt='noimg' style={{maxWidth:'100%', height:'600px', width:'382px', objectFit:'cover'}} />
                                             </SwiperSlide>
                                             ):null
                                         }
                                         {
                                             producto[0].url?.img2?(
-                                                <SwiperSlide>
-                                                    <img src={producto[0].url.img2} alt='noimg' style={{maxWidth:'100%', height:'600px', width:'382px', objectFit:'cover'}} />
+                                                <SwiperSlide style={{display:'flex', justifyContent:''}}>
+                                                    <img className="pcSwiper" src={producto[0].url.img2} alt='noimg' style={{maxWidth:'100%', height:'600px', width:'382px', objectFit:'cover'}} />
                                                 </SwiperSlide>
                                                 ):null
                                         }
@@ -96,6 +98,79 @@ function ProductDetail(){
                     </Grid>
                 </Grid>
             </Grid>
+
+
+
+            {/*  MOBILE */}
+            <Grid key ='24e23e' container sx={{width:'100%', display:{xs:'flex', md:'none'}, justifyContent:'center', marginTop:'1.2rem'}}>
+                <Grid container sx={{width:{xs:'96%',xl:'80%'},marginTop:'8rem', background:'var(--azulOscuro)', color:'white', borderRadius:'10px'}}>
+                    <TittleEfect text={producto[0].name} align='center' width='100%' fontSize={"30px"} margin='2rem 0px'/>
+                           
+                    {/* Container miniaturas */}
+    
+                    {/* <Grid item xs={1} sx={{display:'flex', flexDirection:'column'}}>
+                        {
+                            producto && producto[0] && producto[0].name !== ''?(
+                                    <>
+                                    {
+                                        producto[0].url?.img1?(<img src={producto[0].url.img1} style={{width:"60px", height:'80px', objectFit:'cover', margin:'5px auto',borderRadius:'5px'}}/>):null
+                                    }
+                                    {
+                                        producto[0].url?.img1?(<img src={producto[0].url.img1} style={{width:"60px", height:'80px', objectFit:'cover', margin:'5px auto',borderRadius:'5px'}}/>):null
+                                    }
+                                    {
+                                        producto[0].url?.img1?(<img src={producto[0].url.img1} style={{width:"60px", height:'80px', objectFit:'cover', margin:'5px auto',borderRadius:'5px'}}/>):null
+                                    }
+                                    {
+                                        producto[0].url?.img1?(<img src={producto[0].url.img1} style={{width:"60px", height:'80px', objectFit:'cover', margin:'5px auto',borderRadius:'5px'}}/>):null
+                                    }
+                                    {
+                                        producto[0].url?.img1?(<img src={producto[0].url.img1} style={{width:"60px", height:'80px', objectFit:'cover', margin:'5px auto',borderRadius:'5px'}}/>):null
+                                    }
+                                    {
+                                        producto[0].url?.img1?(<img src={producto[0].url.img1} style={{width:"60px", height:'80px', objectFit:'cover', margin:'5px auto',borderRadius:'5px'}}/>):null
+                                    }
+                         
+                                    </>
+                                ):null
+                        }
+                    </Grid> */}
+    
+                    {/* Imagen principal */}
+                    <Grid item xs={12} >
+                        <Box >
+                            {
+                                producto &&producto[0] && producto[0].name !== ''?(
+                                    <Swiper
+                                    grabCursor
+                                    keyboard={{enabled:true}}
+                                    pagination={{clickable:true}}
+                                    navigation
+                                    loop
+                                    style={{padding:'0 0 3rem 0',}}
+                                    >
+                                        {
+                                            producto[0].url?.img1?(
+                                            <SwiperSlide style={{display:'flex', justifyContent:'center'}}>
+                                                <img className='pcSwiper' src={producto[0].url.img1} alt='noimg' style={{maxWidth:'100%', maxHeight:'500px', width:'auto', objectFit:'cover'}} />
+                                            </SwiperSlide>
+                                            ):null
+                                        }
+                                        {
+                                            producto[0].url?.img2?(
+                                                <SwiperSlide style={{display:'flex', justifyContent:'center'}}>
+                                                    <img className='pcSwiper' src={producto[0].url.img2} alt='noimg' style={{maxWidth:'100%', maxHeight:'500px', width:'auto', objectFit:'cover'}} />
+                                                </SwiperSlide>
+                                                ):null
+                                        }
+                                    </Swiper>
+                                ):null
+                            }
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Grid>
+            </>
         )
     }else{
        return(
